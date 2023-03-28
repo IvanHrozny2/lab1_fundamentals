cd21988ca4ef35f0dfd02efc6513126e8110012a
IvanHrozny2 <96377288+IvanHrozny2@users.noreply.github.com>
2023-03-28 22:05:25 +0300

    Change ProgressIndificatorScreen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -81,5 +83,12 @@ fun MyRadioGroup() {
 
 @Composable
 fun MyFloatingActionButton() {
-    //TODO add your code here
+   FloatingActionButton(
+       onClick = {},
+       backgroundColor = colorResource(id = R.color.purple_700),
+       contentColor = Color.White,
+       content = {
+           Icon(Icons.Filled.Favorite, contentDescription = "Test FAB ")
+       }
+   )
 }
